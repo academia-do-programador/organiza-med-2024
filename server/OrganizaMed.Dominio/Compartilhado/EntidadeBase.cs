491472eb3fd6997a1b90e0e006bb35a556e71f71
Tiago Santini <tiagosantiniathayde@outlook.com>
2024-12-03 12:26:01 -0300

    Implementado domínio e testes de unidade de médicos

---
+++ server/OrganizaMed.Dominio/Compartilhado/EntidadeBase.cs
@@ -0,0 +1,11 @@
+﻿namespace OrganizaMed.Dominio.Compartilhado;
+
+public abstract class EntidadeBase
+{
+    public Guid Id { get; set; }
+
+    protected EntidadeBase()
+    {
+        Id = Guid.NewGuid();
+    }
+}
