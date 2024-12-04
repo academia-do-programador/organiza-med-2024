a3709af4e6a8e342c450ab0657892890b429957f
Tiago Santini <tiagosantiniathayde@outlook.com>
2024-12-04 13:10:08 -0300

    Implementada configuração inicial e dependências da API

---
+++ server/OrganizaMed.WebApi/DependencyInjection.cs
@@ -0,0 +1,137 @@
+﻿using FluentValidation;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
+using OrganizaMed.Aplicacao.ModuloMedico.Commands.Inserir;
+using OrganizaMed.Dominio.Compartilhado;
+using OrganizaMed.Dominio.ModuloMedico;
+using OrganizaMed.Infraestrutura.Orm.Compartilhado;
+using OrganizaMed.Infraestrutura.Orm.ModuloMedico;
+using OrganizaMed.WebApi.Filters;
+using Serilog;
+
+namespace OrganizaMed.WebApi;
+
+public static class DependencyInjection
+{
+    public static void ConfigureDbContext(
+        this IServiceCollection services,
+        IConfiguration config,
+        IWebHostEnvironment environment
+    )
+    {
+        var connectionString = config["SQLSERVER_CONNECTION_STRING"];
+
+        if (connectionString == null)
+            throw new ArgumentNullException("'SQLSERVER_CONNECTION_STRING' não foi fornecida para o ambiente.");
+
+        services.AddDbContext<IContextoPersistencia, OrganizaMedDbContext>(optionsBuilder =>
+        {
+            if (!environment.IsDevelopment())
+                optionsBuilder.EnableSensitiveDataLogging(false);
+
+            optionsBuilder.UseSqlServer(connectionString, dbOptions =>
+            {
+                dbOptions.EnableRetryOnFailure();
+            });
+        });
+    }
+
+    public static void ConfigureRepositories(this IServiceCollection services)
+    {
+        services.AddScoped<IRepositorioMedico, RepositorioMedicoEmOrm>();
+    }
+
+    public static void ConfigureControllersWithFilters(this IServiceCollection services)
+    {
+        services.AddControllers(options =>
+        {
+            options.Filters.Add<ResponseWrapperFilter>();
+        });
+    }
+
+    public static void ConfigureOpenApiAuthHeaders(this IServiceCollection services)
+    {
+        services.AddEndpointsApiExplorer();
+
+        services.AddSwaggerGen(options =>
+        {
+            options.SwaggerDoc("v1", new OpenApiInfo { Title = "organiza-med-api", Version = "v1" });
+
+            options.MapType<TimeSpan>(() => new OpenApiSchema
+            {
+                Type = "string",
+                Format = "time-span",
+                Example = new Microsoft.OpenApi.Any.OpenApiString("00:00:00")
+            });
+
+            options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+            {
+                In = ParameterLocation.Header,
+                Name = "Authorization",
+                Description = "Informe o token JWT no padrão {Bearer token}",
+                Type = SecuritySchemeType.ApiKey,
+                Scheme = "Bearer",
+                BearerFormat = "JWT"
+            });
+
+            options.AddSecurityRequirement(new OpenApiSecurityRequirement
+            {
+                {
+                    new OpenApiSecurityScheme
+                    {
+                        Reference = new OpenApiReference
+                        {
+                            Type = ReferenceType.SecurityScheme,
+                            Id = "Bearer"
+                        }
+                    },
+                    []
+                }
+            });
+        });
+    }
+
+    public static void ConfigureCorsPolicy(this IServiceCollection services, string nomePolitica)
+    {
+        services.AddCors(options =>
+        {
+            options.AddPolicy(name: nomePolitica, policy =>
+            {
+                policy
+                    .AllowAnyOrigin()
+                    .AllowAnyHeader()
+                    .AllowAnyMethod();
+            });
+        });
+    }
+
+    public static void ConfigureFluentValidation(this IServiceCollection services)
+    {
+        services.AddValidatorsFromAssemblyContaining<ValidadorMedico>();
+    }
+
+    public static void ConfigureSerilog(this IServiceCollection services, ILoggingBuilder logging, IConfiguration config)
+    {
+        Log.Logger = new LoggerConfiguration()
+            .Enrich.FromLogContext()
+            .WriteTo.Console()
+            .WriteTo.NewRelicLogs(
+                endpointUrl: "https://log-api.newrelic.com/log/v1",
+                applicationName: "organiza-med-api",
+                licenseKey: config["NEWRELIC_LICENSE_KEY"]
+            )
+            .CreateLogger();
+
+        logging.ClearProviders();
+
+        services.AddLogging(builder => builder.AddSerilog(dispose: true));
+    }
+
+    public static void ConfigureMediatR(this IServiceCollection services)
+    {
+        services.AddMediatR(cfg =>
+        {
+            cfg.RegisterServicesFromAssemblyContaining<InserirMedicoRequest>();
+        });
+    }
+}
---
+++ server/OrganizaMed.WebApi/Program.cs
@@ -0,0 +1,71 @@
+
+using OrganizaMed.WebApi.Config;
+using Serilog;
+
+namespace OrganizaMed.WebApi;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        const string corsPolicyName = "PoliticaCorsOrganizaMed";
+
+        var builder = WebApplication.CreateBuilder(args);
+
+        // Logging [env NEWRELIC_LICENSE_KEY]
+        builder.Services.ConfigureSerilog(builder.Logging, builder.Configuration);
+
+        // Database Provider [env SQLSERVER_CONNECTION_STRING]
+        builder.Services.ConfigureDbContext(builder.Configuration, builder.Environment);
+
+        // Validation
+        builder.Services.ConfigureFluentValidation();
+
+        // Services
+        builder.Services.ConfigureRepositories();
+        builder.Services.ConfigureMediatR();
+
+        // Controllers
+        builder.Services.ConfigureControllersWithFilters();
+
+        // API Documentation 
+        builder.Services.ConfigureOpenApiAuthHeaders();
+
+        // CORS
+        builder.Services.ConfigureCorsPolicy(corsPolicyName);
+
+        var app = builder.Build();
+
+        app.UseGlobalExceptionHandler();
+
+        var migracoesRealizadas = app.AutoMigrateDatabase();
+
+        Log.Debug(migracoesRealizadas
+            ? "Migrações de banco de dados realizadas."
+            : "Nenhuma migração de banco de dados aplicadas."
+        );
+
+        app.UseSwagger();
+
+        app.UseSwaggerUI();
+
+        app.UseHttpsRedirection();
+
+        app.UseCors(corsPolicyName);
+
+        app.UseAuthentication();
+
+        app.UseAuthorization();
+
+        app.MapControllers();
+
+        try
+        {
+            app.Run();
+        }
+        catch (Exception ex)
+        {
+            Log.Fatal("Ocorreu um erro fatal durante a execução da aplicação: {@Excecao}", ex);
+        }
+    }
+}
