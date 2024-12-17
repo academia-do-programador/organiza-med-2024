e26b882cb524debaa5a6bbc57c9ce4fffda25ba0
Tiago Santini <tiagosantiniathayde@outlook.com>
2024-12-17 08:56:38 -0300

    Implementado domínio de paciente

---
+++ server/OrganizaMed.Dominio/ModuloPaciente/Paciente.cs
@@ -0,0 +1,14 @@
+﻿using OrganizaMed.Dominio.Compartilhado;
+using OrganizaMed.Dominio.ModuloAtividade;
+
+namespace OrganizaMed.Dominio.ModuloPaciente;
+
+public class Paciente : EntidadeBase
+{
+    public string Nome { get; set; }
+    public string Cpf { get; set; }
+    public string Email { get; set; }
+    public string Telefone { get; set; }
+    
+    public List<AtividadeMedica> Atividades { get; set; }
+}
