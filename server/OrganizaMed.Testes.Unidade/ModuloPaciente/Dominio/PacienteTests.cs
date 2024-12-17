014470a896228d9983aff739c9c5390d06581dd8
Tiago Santini <tiagosantiniathayde@outlook.com>
2024-12-17 09:38:29 -0300

    Implementados testes de integração de pacientes e criado construtor

--- server/OrganizaMed.Testes.Unidade/ModuloPaciente/Dominio/PacienteTests.cs
+++ server/OrganizaMed.Testes.Unidade/ModuloPaciente/Dominio/PacienteTests.cs
@@ -18,13 +18,8 @@ namespace OrganizaMed.Testes.Unidade.ModuloPaciente.Dominio
         [TestMethod]
         public void Deve_Passar_Quando_Todas_Propriedades_Forem_Validas()
         {
-            var paciente = new Paciente
-            {
-                Nome = "João da Silva",
-                Cpf = "123.456.789-00",
-                Email = "joao.silva@email.com",
-                Telefone = "(11) 91234-5678"
-            };
+            var paciente = new Paciente("João da Silva", "123.456.789-00", "joao.silva@email.com",
+                "(11) 91234-5678");
 
             _validador.TestValidate(paciente).ShouldNotHaveAnyValidationErrors();
         }
@@ -32,13 +27,8 @@ namespace OrganizaMed.Testes.Unidade.ModuloPaciente.Dominio
         [TestMethod]
         public void Deve_Falhar_Quando_Nome_Estiver_Vazio()
         {
-            var paciente = new Paciente
-            {
-                Nome = "",
-                Cpf = "123.456.789-00",
-                Email = "joao.silva@email.com",
-                Telefone = "(11) 91234-5678"
-            };
+            var paciente = new Paciente("", "123.456.789-00", "joao.silva@email.com",
+                "(11) 91234-5678");
 
             var result = _validador.TestValidate(paciente);
 
@@ -49,13 +39,8 @@ namespace OrganizaMed.Testes.Unidade.ModuloPaciente.Dominio
         [TestMethod]
         public void Deve_Falhar_Quando_Nome_For_Menor_Que_Tres_Caracteres()
         {
-            var paciente = new Paciente
-            {
-                Nome = "Ab",
-                Cpf = "123.456.789-00",
-                Email = "joao.silva@email.com",
-                Telefone = "(11) 91234-5678"
-            };
+            var paciente = new Paciente("Ab", "123.456.789-00", "joao.silva@email.com",
+                "(11) 91234-5678");
 
             var result = _validador.TestValidate(paciente);
 
@@ -66,13 +51,7 @@ namespace OrganizaMed.Testes.Unidade.ModuloPaciente.Dominio
         [TestMethod]
         public void Deve_Falhar_Quando_Cpf_Estiver_Vazio()
         {
-            var paciente = new Paciente
-            {
-                Nome = "João da Silva",
-                Cpf = "",
-                Email = "joao.silva@email.com",
-                Telefone = "(11) 91234-5678"
-            };
+            var paciente = new Paciente("João da Silva", "", "joao.silva@email.com", "(11) 91234-5678");
 
             var result = _validador.TestValidate(paciente);
 
@@ -83,13 +62,8 @@ namespace OrganizaMed.Testes.Unidade.ModuloPaciente.Dominio
         [TestMethod]
         public void Deve_Falhar_Quando_Cpf_For_Invalido()
         {
-            var paciente = new Paciente
-            {
-                Nome = "João da Silva",
-                Cpf = "12345678900", // Sem o formato correto
-                Email = "joao.silva@email.com",
-                Telefone = "(11) 91234-5678"
-            };
+            var paciente = new Paciente("João da Silva", "12345678900", // Sem o formato correto
+                "joao.silva@email.com", "(11) 91234-5678");
 
             var result = _validador.TestValidate(paciente);
 
@@ -100,13 +74,8 @@ namespace OrganizaMed.Testes.Unidade.ModuloPaciente.Dominio
         [TestMethod]
         public void Deve_Falhar_Quando_Email_Estiver_Vazio()
         {
-            var paciente = new Paciente
-            {
-                Nome = "João da Silva",
-                Cpf = "123.456.789-00",
-                Email = "",
-                Telefone = "(11) 91234-5678"
-            };
+            var paciente = new Paciente("João da Silva", "123.456.789-00", "",
+                "(11) 91234-5678");
 
             var result = _validador.TestValidate(paciente);
 
@@ -117,13 +86,8 @@ namespace OrganizaMed.Testes.Unidade.ModuloPaciente.Dominio
         [TestMethod]
         public void Deve_Falhar_Quando_Email_For_Invalido()
         {
-            var paciente = new Paciente
-            {
-                Nome = "João da Silva",
-                Cpf = "123.456.789-00",
-                Email = "joao.email.invalido",
-                Telefone = "(11) 91234-5678"
-            };
+            var paciente = new Paciente("João da Silva", "123.456.789-00", "joao.email.invalido",
+                "(11) 91234-5678");
 
             var result = _validador.TestValidate(paciente);
 
@@ -134,13 +98,8 @@ namespace OrganizaMed.Testes.Unidade.ModuloPaciente.Dominio
         [TestMethod]
         public void Deve_Falhar_Quando_Telefone_Estiver_Vazio()
         {
-            var paciente = new Paciente
-            {
-                Nome = "João da Silva",
-                Cpf = "123.456.789-00",
-                Email = "joao.silva@email.com",
-                Telefone = ""
-            };
+            var paciente = new Paciente("João da Silva", "123.456.789-00", "joao.silva@email.com",
+                "");
 
             var result = _validador.TestValidate(paciente);
 
@@ -151,13 +110,9 @@ namespace OrganizaMed.Testes.Unidade.ModuloPaciente.Dominio
         [TestMethod]
         public void Deve_Falhar_Quando_Telefone_For_Invalido()
         {
-            var paciente = new Paciente
-            {
-                Nome = "João da Silva",
-                Cpf = "123.456.789-00",
-                Email = "joao.silva@email.com",
-                Telefone = "11912345678" // Sem o formato correto
-            };
+            var paciente = new Paciente("João da Silva", "123.456.789-00", "joao.silva@email.com",
+                "11912345678" // Sem o formato correto
+            );
 
             var result = _validador.TestValidate(paciente);
 
